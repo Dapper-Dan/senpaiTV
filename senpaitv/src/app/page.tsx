0effb664588da96d805d761a76c773583782a9a3
Dapper Dan <daniel.ray.lancaster@gmail.com>
2025-06-03 11:37:21 -0700

    add hero to home

--- senpaitv/src/app/page.tsx
+++ senpaitv/src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className="">
       <main className="flex flex-col gap-[32px]">
-        <Image src={"/images/solo_leveling_hero3.jpeg"} className="home-banner" alt="Solo Leveling Hero" width={1000} height={1000} style={{ objectFit: "cover", width: "100%", height: "100%" }} />
+        <div className={styles.homeBanner}>
+          <Image src={"/images/solo_leveling_hero3.jpeg"} alt="Solo Leveling Hero" width={1000} height={1000} style={{ width: "100%", height: "100%" }} />
+        </div>
         <p>
           SenpaiTV is a platform for watching anime.
         </p>
