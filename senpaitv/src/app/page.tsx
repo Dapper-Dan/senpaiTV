ff14676007838c3872e10b66f4e96a155190b1ff
Dapper Dan <daniel.ray.lancaster@gmail.com>
2025-06-02 22:24:18 -0700

    add assets

--- senpaitv/src/app/page.tsx
+++ senpaitv/src/app/page.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="">
       <main className="flex flex-col gap-[32px]">
+        <Image src={"/images/solo_leveling_hero3.jpeg"} className="home-banner" alt="Solo Leveling Hero" width={1000} height={1000} style={{ objectFit: "cover", width: "100%", height: "100%" }} />
         <p>
           SenpaiTV is a platform for watching anime.
         </p>
--- senpaitv/src/components/Header.tsx
+++ senpaitv/src/components/Header.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Header() {
   return (
     <header className="flex items-center gap-6 px-8 py-4 bg-light-black">
-      <Image src={"/images/senpai_logo.png"} className="profile-icon" alt="profile" width={50} height={50}/>
+      <Image src={"/images/senpai_logo.png"} className="profile-icon" alt="senpaitv logo" width={50} height={50}/>
       <h1 className="text-3xl font-bold">SenpaiTV</h1>
       <ul className="flex gap-4">
         <li className="text-gray">
@@ -20,7 +20,7 @@ export default function Header() {
       </ul>
       <Searchbar />
       <div className="rounded-full bg-gray w-10 h-10 overflow-hidden">
-        <Image src={"/images/senpai_logo.png"} className="profile-icon" alt="profile" width={50} height={50}/>
+        <Image src={"/images/senpai_logo.png"} className="profile-icon" alt="profile icon" width={50} height={50}/>
       </div>
     </header>
   );  
