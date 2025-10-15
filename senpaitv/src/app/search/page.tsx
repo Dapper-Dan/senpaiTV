2cf47d50516117c117bb7f9a6b378b8952902071
Dapper Dan <daniel.ray.lancaster@gmail.com>
2025-10-14 18:53:13 -0700

    update searchbar

--- senpaitv/src/app/search/page.tsx
+++ senpaitv/src/app/search/page.tsx
@@ -1,9 +1,10 @@
 import Searchbar from "@/components/searchBar/Searchbar";
+import styles from "./search.module.css";
 
 export default function Search({searchParams}: {searchParams: {q: string}}) {
   const query = searchParams.q || '';
   return (
-    <div className="page-container">
+    <div className={styles.searchPage}>
       <Searchbar defaultValue={query} />
       {/* {query && <SearchResults query={query} />} */}
     </div>
