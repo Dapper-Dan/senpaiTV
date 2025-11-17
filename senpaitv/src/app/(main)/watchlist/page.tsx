cece60b797bb1c3d6948f3e13956451ac69216c7
Dapper Dan <daniel.ray.lancaster@gmail.com>
2025-11-17 10:34:07 -0800

    stop watchlist from static rendering

--- senpaitv/src/app/(main)/watchlist/page.tsx
+++ senpaitv/src/app/(main)/watchlist/page.tsx
@@ -4,6 +4,8 @@ import WatchlistGrid from '@/components/watchlist/WatchlistGrid';
 import { notFound } from 'next/navigation';
 import AutoSyncWatchlist from '@/components/watchlist/AutoSyncWatchlist';
 
+export const dynamic = 'force-dynamic';
+
 export default async function WatchlistPage() {
   const watchlistItems = await getUserWatchlist();
 
